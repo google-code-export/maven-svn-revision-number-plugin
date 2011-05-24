359caec5778435f166305cd2753f485f47a25799
oleg.estekhin <oleg.estekhin@e10e435e-a6a6-11de-8ba5-91705c1537ed>
2011-05-24 07:39:38 +0000

    bumping dependencies&plugin versions bumping copyyear

--- src/main/java/oe/maven/plugins/revision/EntryStatusSymbols.java
+++ src/main/java/oe/maven/plugins/revision/EntryStatusSymbols.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2009-2010, Oleg Estekhin
+ * Copyright (c) 2009-2011, Oleg Estekhin
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
