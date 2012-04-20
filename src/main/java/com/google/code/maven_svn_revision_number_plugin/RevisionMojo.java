fb25013c82b10d827367fcd1a7fdd5eb4a414154
oleg.estekhin@gmail.com <oleg.estekhin@gmail.com@e10e435e-a6a6-11de-8ba5-91705c1537ed>
2012-04-20 07:19:33 +0000

    removed a clause from the bsd license , now it is The BSD 2-Clause License as in the http://www.opensource.org/licenses/BSD-2-Clause  updated documentation with small format/syntax corrections

--- src/main/java/com/google/code/maven_svn_revision_number_plugin/RevisionMojo.java
+++ src/main/java/com/google/code/maven_svn_revision_number_plugin/RevisionMojo.java
@@ -7,24 +7,21 @@
  *
  *  * Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the distribution.
- *  * Neither the names of the copyright holders nor the names of their
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.google.code.maven_svn_revision_number_plugin;
@@ -100,7 +97,7 @@ public class RevisionMojo extends AbstractMojo {
      * &lt;/entries&gt;
      * </pre>
      * <p/>
-     * If entries configuration is not specified then the goal will operate on the default entry with the entry path
+     * If the entries configuration is not specified then the goal will operate on the default entry with the entry path
      * equal to the project basedir and the properties prefix equal to the project artifactId.
      *
      * @parameter
@@ -110,13 +107,13 @@ public class RevisionMojo extends AbstractMojo {
     /**
      * Specifies whether the goal runs in verbose mode.
      *
-     * @parameter
+     * @parameter default-value="false"
      */
     private boolean verbose;
 
 
     /**
-     * Indicates whether the build will continue even if there are errors related to obtaining the svn revision.
+     * Specifies whether the build should stop or continue if there are errors related to obtaining the svn revision.
      *
      * @parameter expression="${svn-revision-number.failOnError}" default-value="true"
      */
